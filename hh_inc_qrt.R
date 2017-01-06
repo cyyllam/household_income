51dc785b9e22a2023f4f347d6ba9ddda6c3db81b
Christy Lam <cyyllam@gmail.com>
2017-01-06 12:05:31 -0800

    computes multiple hh tables

--- hh_inc_qrt.R
+++ hh_inc_qrt.R
@@ -1,28 +1,40 @@
 # This script reports household income statistics and income quartiles
 # requires 'income' column
-# user must set f.dir (where data resides) and file/ext name
+# user must set f.dir (where data resides)
 
+library(stringr)
 library(plotly) # version 4.0 and up
+options(pandoc.stack.size="2000m")
 
 # read hh income file
 f.dir <- "C:/Users/Christy/Desktop/household_income"
-f <- "hh_2014"
-ext <- ".csv"
-hh.file <- read.table(file.path(f.dir, paste0(f, ext)), header = TRUE, sep = ',')
+files <- list.files(f.dir, pattern = '.csv')
 
-# print quartile summary
-inc_summary <- summary(hh.file$income)
-capture.output(inc_summary, file = file.path(f.dir, paste0(f, "_incsum.txt")))
+income.table <- NULL
 
+for (f in 1:length(files)){
+  hh.file <- NULL
+  year <- NULL
+  inc_summary <- NULL
+  hh.file <- read.table(file.path(f.dir, paste0(files[f])), header = TRUE, sep = ',')
+  file.name <- unlist(strsplit(files[f], "[.]"))[[1]]
+  
+  # print quartile summary
+  inc_summary <- summary(hh.file$income)
+  capture.output(inc_summary, file = file.path(f.dir, paste0(file.name, "_incsum.txt")))
+  
+  # build income table
+  year <- str_match(files[f], ".(\\d+).")[,2]
+  hh.file$year <- year
+  ifelse (is.null(income.table), income.table <- hh.file, income.table <- rbind(income.table, hh.file))
+}
 
 # plot
-p <- plot_ly(hh.file, 
+p <- plot_ly(income.table, 
              y = ~income,
-             name = '2014',
+             color = ~year,
              type = "box") %>%
   layout(font = list(family="Segoe UI", size = 13.5))
 
-print(p)
-
-html.file <- paste0(f, "_incsum_boxplot.html")
+html.file <- paste0("income_summary_boxplot.html")
 htmlwidgets::saveWidget(p, file.path(f.dir, html.file))
